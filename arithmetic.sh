7f08d76070a8abc62e4b51337eca818f6af5d7f5
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-20 16:51:26 +0530

    Added:calculated result of a+b*c

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 echo "computes different arithmetic expression"
 
+#input from user
 read -p "enter number:" a
 read -p "enter number:" b
 read -p "enter number:" c
+
+#calculate expression
+result1=`echo "$a+$b*$c"| bc`
+
