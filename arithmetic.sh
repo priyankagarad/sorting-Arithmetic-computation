f5b074c87f714f331690b489baae98f807a558e0
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-20 18:24:16 +0530

    Added:stored the result of every computation in dictionary

--- arithmetic.sh
+++ arithmetic.sh
@@ -9,7 +9,17 @@ read -p "enter number:" c
 #calculate expression
 result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
-result3=`echo "$c+$a/$b" | bc -l`
-result4=`echo "$a%$b+$c" | bc -l`
+result3=`echo "scale=2; $c+$a/$b" | bc -l`
+result4=`echo "scale=2; $a%$b+$c" | bc -l`
+
+#create Dictionary
+declare -A dictionary
+dictionary[1]=$result1
+dictionary[2]=$result2
+dictionary[3]=$result3
+dictionary[4]=$result4
+
+echo "${!dictionary[@]}"
+echo "${dictionary[@]}"
 
 
