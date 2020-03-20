b9ecef111ec338a20b0ea7a5fe667d0ac8ee9b39
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-20 17:22:17 +0530

    Added:calculated expression:c+a/b

--- arithmetic.sh
+++ arithmetic.sh
@@ -7,6 +7,8 @@ read -p "enter number:" b
 read -p "enter number:" c
 
 #calculate expression
-result1=`echo "$a+$b*$c"| bc`
-result2=`echo "$a*$b+$c"| bc`
+result1=`echo "$a+$b*$c" | bc`
+result2=`echo "$a*$b+$c" | bc`
+result3=`echo "$c+$a/$b" | bc -l`
+
 
