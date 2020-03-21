2a36b90f6b2d60bc08192cf5e666b334307ca810
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-21 13:10:12 +0530

    Added:sorted result value in ascending order

--- arithmetic.sh
+++ arithmetic.sh
@@ -12,7 +12,7 @@ result2=`echo " $a*$b+$c" | bc `
 result3=`echo " $c+$a/$b" | bc `
 result4=`echo " $a%$b+$c" | bc `
 
-#create Dictionary
+#create Dictionary and store result in dictionary
 declare -A dictionary
 dictionary[1]=$result1
 dictionary[2]=$result2
@@ -48,5 +48,19 @@ do
 done
 echo "${array[@]}"
 
+#sorted array in ascending order
+for (( i=1;i<=4;i++ ))
+do
+	for (( j=$((i+1));j<=4;j++ ))
+	do
+		if [[ ${array[$i]} -gt ${array[$j]} ]]
+		then
+			temp=${array[$j]}
+			array[$j]=${array[$i]}
+			array[$i]=$temp
+		fi
+	done
+done
+echo "${array[@]}"
 
 
